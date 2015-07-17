8bdd0e6ac196b6bdf4d390da0e97aa222f6e2a31
Chris Pennello <git@chrispennello.com>
2015-07-17 14:04:31 -0700

    Adds extra parser options as flags.

--- main.go
+++ main.go
@@ -4,14 +4,23 @@
 // structure of Go programs as well as the behavior of the library
 // parser.
 //
-// If a command line argument is specified, it will be used as the input
-// file.  If no arguments are specified, the input will be read from
-// standard in.
+// If a positional command line argument is specified, it will be used
+// as the input file.  If no positional arguments are specified, the
+// input will be read from standard in.
 //
-//	usage: goparse [file]
+// There are other options as well that will enable other parser
+// functionality.
+//
+//	usage: goparse [-h] [options] [file]
+//	  -all-errors=false: report all errors (not just the first 10
+//	                     on different lines)
+//	  -declaration-errors=false: report declaration errors
+//	  -imports-only=false: stop parsing after import declarations
+//	  -parse-comments=false: parse comments and add them to AST
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io"
 	"log"
@@ -22,27 +31,50 @@ import (
 	"go/token"
 )
 
+var mode parser.Mode
+
+func init() {
+	mode = parser.Trace
+	importsOnly := flag.Bool("imports-only", false, "stop parsing after import declarations")
+	parseComments := flag.Bool("parse-comments", false, "parse comments and add them to AST")
+	declarationErrors := flag.Bool("declaration-errors", false, "report declaration errors")
+	allErrors := flag.Bool("all-errors", false, "report all errors (not just the first 10 on different lines)")
+	flag.Parse()
+	if (*importsOnly) {
+		mode |= parser.ImportsOnly
+	}
+	if (*parseComments) {
+		mode |= parser.ParseComments
+	}
+	if (*declarationErrors) {
+		mode |= parser.DeclarationErrors
+	}
+	if (*allErrors) {
+		mode |= parser.AllErrors
+	}
+}
+
 func main() {
 	var filename string
 	var src io.Reader
 	var err error
 
-	if len(os.Args) == 1 {
+	fargs := flag.Args()
+	if len(fargs) == 0 {
 		filename = "-"
 		src = os.Stdin
-	} else if len(os.Args) == 2 {
-		filename = os.Args[1]
+	} else if len(fargs) == 1 {
+		filename = fargs[0]
 		src, err = os.Open(filename)
 		if err != nil {
 			log.Fatal(err)
 		}
 	} else {
-		fmt.Printf("usage: %s [file]\n", path.Base(os.Args[0]))
+		fmt.Printf("usage: %s [-h] [options] [file]\n", path.Base(os.Args[0]))
 		os.Exit(2)
 	}
 
 	fset := token.NewFileSet()
-	mode := parser.Trace
 	_, err = parser.ParseFile(fset, filename, src, mode)
 	if err != nil {
 		log.Fatal(err)
